6d1682d5f77448ff8c40a8157e02af3ccc05c9ad
GastonQuelali (Catastro) <gquelali@catastrocbba.com>
2026-05-17 09:43:26 -0400

    refactor: simplify entry point for Storybook toggle

--- index.tsx
+++ index.tsx
@@ -1,1 +1,9 @@
-import 'expo-router/entry';
+import { Platform } from 'react-native';
+
+const STORYBOOK_ENABLED = process.env.STORYBOOK_ENABLED === 'true';
+
+if (STORYBOOK_ENABLED && Platform.OS !== 'web') {
+  require('./.rnstorybook');
+} else {
+  require('expo-router/entry');
+}
